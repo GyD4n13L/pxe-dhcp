b5da8ed448120f348d2a93feed98e0be32770155
GyD4n13L <32672899+GyD4n13L@users.noreply.github.com>
2018-11-27 09:29:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo apt update
 sudo apt-get install isc-dhcp-server -y \
-      apache2 -y \
+echo "Írd be az interfaces nevét, amelyiken a DHCP szervert akarod futtatni, majd nyomj egy ENTER-t!"
+read d
+echo 'INTERFACES= "$d"'
+
+apache2 -y \
       tftpd-hpa -y \
       inetutils-inetd -y \
